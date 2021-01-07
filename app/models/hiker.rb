124a9a8d016b2530d08fd5342a89e48139f8a9fe
Mai Pham <maitphm@gmail.com>
2021-01-07 13:48:14 -0500

    fixed favorited

--- app/models/hiker.rb
+++ app/models/hiker.rb
@@ -12,6 +12,14 @@ class Hiker < ApplicationRecord
     # validates :password, presence: true
     # validates :password, length: { in: 6..12, message: "should be between 6 - 12 characters long" }
 
+    def display_rating(trail_id)
+       review = self.reviews.find { |review| review.trail_id == trail_id }
+       if review 
+        review.rating
+       else 
+        "Have not rated"
+       end 
+    end 
 end
 
 
