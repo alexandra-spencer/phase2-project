cc527b4d7c4e5821ac26e8df287b1661bba56ef2
Mai Pham <maitphm@gmail.com>
2021-06-04 23:56:40 -0400

    cleaned up code

--- app/models/hiker.rb
+++ app/models/hiker.rb
@@ -9,9 +9,7 @@ class Hiker < ApplicationRecord
     validates :name, presence: true
     validates :username, presence: true
     validates :username, uniqueness: true
-    # validates :password, presence: true
-    # validates :password, length: { in: 6..12, message: "should be between 6 - 12 characters long" }
-
+    
     def display_rating(trail_id)
        review = self.reviews.find { |review| review.trail_id == trail_id }
        if review 
